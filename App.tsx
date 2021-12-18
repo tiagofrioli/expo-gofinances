aeb04d16b93bd167ba88a1dbe196d3bcdb24c496
Tiago <tiago.frioli@gmail.com>
2021-12-18 00:38:10 -0300

    creation of record view header

--- App.tsx
+++ App.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components/native';
-import theme from './src/global/styles/theme';
-import AppLoading from 'expo-app-loading'; '@expo-app-loading';
-import { useFonts, Poppins_400Regular, Poppins_700Bold, Poppins_500Medium } from '@expo-google-fonts/poppins';
-import Dashboard from './src/pages/Dashboard';
-
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/global/styles/theme";
+import AppLoading from "expo-app-loading";
+("@expo-app-loading");
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold,
+  Poppins_500Medium,
+} from "@expo-google-fonts/poppins";
+import Dashboard from "./src/pages/Dashboard";
+import Register from "./src/pages/Register";
 
 export default function App() {
-  const[ fontsLoaded ] = useFonts({
-    Poppins_400Regular, Poppins_700Bold, Poppins_500Medium
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+    Poppins_500Medium,
   });
 
-  if(!fontsLoaded)
-    return <AppLoading />
+  if (!fontsLoaded) return <AppLoading />;
 
   return (
-   <ThemeProvider theme={theme}>
-     <Dashboard />
-   </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <Register />
+    </ThemeProvider>
   );
 }
-
---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Container, Header, Title } from "./styles";
+
+const Register: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+    </Container>
+  );
+};
+
+export default Register;
